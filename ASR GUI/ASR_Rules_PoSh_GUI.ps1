b78f8007ed76b7e93994ed0695f20b271a3b9a73
hemaurer <pretender@gentlemansclub.de>
2024-11-12 23:20:27 +0100

    Update ASR_Rules_PoSh_GUI.ps1
    + Added ASR Rule "Block rebooting machine in Safe Mode (preview)"
    + Added ASR Rule "Block use of copied or impersonated system tools (preview)"
    + minor translation changes

--- ASR GUI/ASR_Rules_PoSh_GUI.ps1
+++ ASR GUI/ASR_Rules_PoSh_GUI.ps1
@@ -7,7 +7,7 @@
         xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
         xmlns:local="clr-namespace:ASR_GUI_Radio"
         mc:Ignorable="d"
-        Title="ASR Rules PoSh GUI - Version 1.1 - 23rd Dec 2021" Width="900" Height="620" MaxHeight="620" MaxWidth="900" MinHeight="620" MinWidth="900">
+        Title="ASR Rules PoSh GUI - Version 1.2 - 14rd Aug 2024" Width="900" Height="680" MaxHeight="680" MaxWidth="900" MinHeight="680" MinWidth="900">
     <StackPanel>
         <Menu  IsMainMenu="true">
             <MenuItem Header="_File">
@@ -248,6 +248,34 @@
             <RadioButton x:Name="VulnDriversB" Content="Enabled" VerticalAlignment="Center" Grid.Column="3" HorizontalAlignment="Left" Grid.IsSharedSizeScope="True" Panel.ZIndex="2" />
             <RadioButton x:Name="VulnDriversW" Content="Warn" VerticalAlignment="Center" Grid.Column="4" HorizontalAlignment="Left" Grid.IsSharedSizeScope="True" Panel.ZIndex="2" />
         </Grid>
+        <Grid>
+            <Grid.ColumnDefinitions>
+                <ColumnDefinition Width="5*"/>
+                <ColumnDefinition Width="*"/>
+                <ColumnDefinition Width="*"/>
+                <ColumnDefinition Width="*"/>
+                <ColumnDefinition/>
+            </Grid.ColumnDefinitions>
+            <Label Content="Block rebooting machine in Safe Mode (preview)" Grid.Column="0" />
+            <RadioButton x:Name="RebootSafeD" Content="Disabled" VerticalAlignment="Center"  Grid.Column="1" HorizontalAlignment="Left" Grid.IsSharedSizeScope="True" Panel.ZIndex="2" />
+            <RadioButton x:Name="RebootSafeA" Content="Audit" VerticalAlignment="Center"  Grid.Column="2" HorizontalAlignment="Left" Grid.IsSharedSizeScope="True" Panel.ZIndex="2" />
+            <RadioButton x:Name="RebootSafeB" Content="Enabled" VerticalAlignment="Center" Grid.Column="3" HorizontalAlignment="Left" Grid.IsSharedSizeScope="True" Panel.ZIndex="2" />
+            <RadioButton x:Name="RebootSafeW" Content="Warn" VerticalAlignment="Center" Grid.Column="4" HorizontalAlignment="Left" Grid.IsSharedSizeScope="True" Panel.ZIndex="2" />
+        </Grid>
+        <Grid>
+            <Grid.ColumnDefinitions>
+                <ColumnDefinition Width="5*"/>
+                <ColumnDefinition Width="*"/>
+                <ColumnDefinition Width="*"/>
+                <ColumnDefinition Width="*"/>
+                <ColumnDefinition/>
+            </Grid.ColumnDefinitions>
+            <Label Content="Block use of copied or impersonated system tools (preview)" Grid.Column="0" />
+            <RadioButton x:Name="ImpersonSysToolsD" Content="Disabled" VerticalAlignment="Center"  Grid.Column="1" HorizontalAlignment="Left" Grid.IsSharedSizeScope="True" Panel.ZIndex="2" />
+            <RadioButton x:Name="ImpersonSysToolsA" Content="Audit" VerticalAlignment="Center"  Grid.Column="2" HorizontalAlignment="Left" Grid.IsSharedSizeScope="True" Panel.ZIndex="2" />
+            <RadioButton x:Name="ImpersonSysToolsB" Content="Enabled" VerticalAlignment="Center" Grid.Column="3" HorizontalAlignment="Left" Grid.IsSharedSizeScope="True" Panel.ZIndex="2" />
+            <RadioButton x:Name="ImpersonSysToolsW" Content="Warn" VerticalAlignment="Center" Grid.Column="4" HorizontalAlignment="Left" Grid.IsSharedSizeScope="True" Panel.ZIndex="2" />
+        </Grid>
         <Grid Height="40" VerticalAlignment="Center">
             <Grid.ColumnDefinitions>
                 <ColumnDefinition Width="5*"/>
@@ -278,13 +306,13 @@
 
 
 
-"@ -replace 'mc:Ignorable="d"','' -replace "x:N",'N' -replace '^<Win.*', '<Window' #-replace wird benötigt, wenn XAML aus Visual Studio kopiert wird.
-#XAML laden
+"@ -replace 'mc:Ignorable="d"','' -replace "x:N",'N' -replace '^<Win.*', '<Window' #-replace is necessary, if XAML was copied from Visual Studio.
+#Read XAML
 [void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
 try{
    $Form=[Windows.Markup.XamlReader]::Load( (New-Object System.Xml.XmlNodeReader $XAML) )
 } catch {
-   Write-Host "Windows.Markup.XamlReader konnte nicht geladen werden. Mögliche Ursache: ungültige Syntax oder fehlendes .net"
+   Write-Host "Windows.Markup.XamlReader could not be loaded. Possible Reason: Syntax or missing .net"
 }
 
 #Write-Host ====================================== ASR RUles Powershell GUI Live Log ======================================
@@ -301,7 +329,7 @@ function ASRReset {
     $RulesIdsArray += $RulesIds
 
     ##Uncheck RadioButtons
-    $RadioArray = "OAppChildProc", "ObfusScript", "MacroAPI", "OfficeCreateExe", "OfficeCodeInj", "ScriptDwnlExe", "MailExe", "FileCriteria", "Ransomware", "lsass", "prcCreate", "USB", "OCommChildProc", "PDFChildProc", "WMIPers", "VulnDrivers"
+    $RadioArray = "OAppChildProc", "ObfusScript", "MacroAPI", "OfficeCreateExe", "OfficeCodeInj", "ScriptDwnlExe", "MailExe", "FileCriteria", "Ransomware", "lsass", "prcCreate", "USB", "OCommChildProc", "PDFChildProc", "WMIPers", "VulnDrivers", "RebootSafe", "ImpersonSysTools"
     for ($i=0; $i -lt $RadioArray.Length; $i++){
         $RadioCheckA = $RadioArray[$i]+"A"
         $RadioCheckD = $RadioArray[$i]+"D"
@@ -353,6 +381,9 @@ function ASRReset {
         ElseIf ($RulesIdsArray[$counter] -eq "7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c"){$ASR = "PDFChildProc"}
         ElseIf ($RulesIdsArray[$counter] -eq "e6db77e5-3df2-4cf1-b95a-636979351e5b"){$ASR = "WMIPers"}
         ElseIf ($RulesIdsArray[$counter] -eq "56a863a9-875e-4185-98a7-b882c64b5ce5"){$ASR = "VulnDrivers"}
+        ElseIf ($RulesIdsArray[$counter] -eq "33ddedf1-c6e0-47cb-833e-de6133960387"){$ASR = "RebootSafe"}
+        ElseIf ($RulesIdsArray[$counter] -eq "c0033c00-d16d-4114-a5a0-dc9b3a7d2ceb"){$ASR = "ImpersonSysTools"}
+        
         ## Checking the Action Mode/Radio Button
         If ($RulesActions[$counter] -eq 0){$Radio = $ASR+"D" }
         ElseIf ($RulesActions[$counter] -eq 1){ $Radio = $ASR+"B"}
@@ -384,8 +415,8 @@ $btnSave.Add_Click({
 
     #Write-Host ====================================== Set ASR Rules ======================================
     #Write-Host 
-    $RadioArray = "OAppChildProc", "ObfusScript", "MacroAPI", "OfficeCreateExe", "OfficeCodeInj", "ScriptDwnlExe", "MailExe", "FileCriteria", "Ransomware", "lsass", "prcCreate", "USB", "OCommChildProc", "PDFChildProc", "WMIPers", "VulnDrivers"
-    $ASRGUIDArray = "D4F940AB-401B-4EFC-AADC-AD5F3C50688A", "5BEB7EFE-FD9A-4556-801D-275E5FFC04CC", "92E97FA1-2EDF-4476-BDD6-9DD0B4DDDC7B", "3B576869-A4EC-4529-8536-B80A7769E899", "75668C1F-73B5-4CF0-BB93-3ECF5CB7CC84", "D3E037E1-3EB8-44C8-A917-57927947596D", "BE9BA2D9-53EA-4CDC-84E5-9B1EEEE46550", "01443614-cd74-433a-b99e-2ecdc07bfc25", "c1db55ab-c21a-4637-bb3f-a12568109d35", "9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2", "d1e49aac-8f56-4280-b9ba-993a6d77406c", "b2b3f03d-6a65-4f7b-a9c7-1c7ef74a9ba4", "26190899-1602-49e8-8b27-eb1d0a1ce869", "7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c", "e6db77e5-3df2-4cf1-b95a-636979351e5b", "56a863a9-875e-4185-98a7-b882c64b5ce5"
+    $RadioArray = "OAppChildProc", "ObfusScript", "MacroAPI", "OfficeCreateExe", "OfficeCodeInj", "ScriptDwnlExe", "MailExe", "FileCriteria", "Ransomware", "lsass", "prcCreate", "USB", "OCommChildProc", "PDFChildProc", "WMIPers", "VulnDrivers", "RebootSafe", "ImpersonSysTools"
+    $ASRGUIDArray = "D4F940AB-401B-4EFC-AADC-AD5F3C50688A", "5BEB7EFE-FD9A-4556-801D-275E5FFC04CC", "92E97FA1-2EDF-4476-BDD6-9DD0B4DDDC7B", "3B576869-A4EC-4529-8536-B80A7769E899", "75668C1F-73B5-4CF0-BB93-3ECF5CB7CC84", "D3E037E1-3EB8-44C8-A917-57927947596D", "BE9BA2D9-53EA-4CDC-84E5-9B1EEEE46550", "01443614-cd74-433a-b99e-2ecdc07bfc25", "c1db55ab-c21a-4637-bb3f-a12568109d35", "9e6c4e1f-7d60-472f-ba1a-a39ef669e4b2", "d1e49aac-8f56-4280-b9ba-993a6d77406c", "b2b3f03d-6a65-4f7b-a9c7-1c7ef74a9ba4", "26190899-1602-49e8-8b27-eb1d0a1ce869", "7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c", "e6db77e5-3df2-4cf1-b95a-636979351e5b", "56a863a9-875e-4185-98a7-b882c64b5ce5", "33ddedf1-c6e0-47cb-833e-de6133960387", "c0033c00-d16d-4114-a5a0-dc9b3a7d2ceb"
     $ASRGUIDString = ""
     
     for ($i=0; $i -lt $RadioArray.Length; $i++){
@@ -529,6 +560,8 @@ $btnReport.Add_Click({
         ElseIf ($RulesIdsArray[$counter] -eq "7674ba52-37eb-4a4f-a9a1-f0f9a1619a2c"){$RuleName = "Block Adobe Reader from creating child processes"}
         ElseIf ($RulesIdsArray[$counter] -eq "e6db77e5-3df2-4cf1-b95a-636979351e5b"){$RuleName = "Block persistence through WMI event subscription"}
         ElseIf ($RulesIdsArray[$counter] -eq "56a863a9-875e-4185-98a7-b882c64b5ce5"){$RuleName = "Block abuse of exploited vulnerable signed drivers"}
+        ElseIf ($RulesIdsArray[$counter] -eq "33ddedf1-c6e0-47cb-833e-de6133960387"){$RuleName = "Block rebooting machine in Safe Mode (preview)"}
+        ElseIf ($RulesIdsArray[$counter] -eq "c0033c00-d16d-4114-a5a0-dc9b3a7d2ceb"){$RuleName = "Block use of copied or impersonated system tools (preview)"}
         ## Check the Action type
         If ($RulesActions[$counter] -eq 0){$RuleAction = "Disabled"}
         ElseIf ($RulesActions[$counter] -eq 1){$RuleAction = "Block   "}
